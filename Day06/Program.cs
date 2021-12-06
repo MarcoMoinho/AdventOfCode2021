9083dbb15947249b632539196c8e2c7bcdc8f252
MarcoMoinho <marco.moinho@gmail.com>
2021-12-06 09:43:49 +0100

    Day 06 cleanup

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,20 +1,14 @@
 ﻿// Day 06
 var inputs = System.IO.File.ReadAllLines("..\\..\\..\\..\\06.txt");
 
-Int64 SumFish(ref Int64[] fish) {
-    Int64 sum = 0;
-    for (var i = 0; i < 9; i++) sum += fish[i];
-    return sum;
-}
-
 Int64[] fish = new Int64[9];
 foreach (var i in inputs[0].Split(',')) fish[Int32.Parse(i)] += 1;
 
 for (var day = 0; day < 256; day++) {
     var newFish = fish[0];                              // these will spawn new fish
-    for (int d = 1; d < 9; d++) fish[d-1] = fish[d];    // move them to the next bucket
+    Array.Copy(fish, 1, fish, 0, fish.Length - 1);      // move them to the next bucket 
     fish[8] = newFish;                                  // add new fish
     fish[6] += newFish;                                 // respwan old fish
-    if (day == 79)  Console.WriteLine("Part 1: {0}", SumFish(ref fish));
-    if (day == 255) Console.WriteLine("Part 2: {0}", SumFish(ref fish));
+    if (day == 79)  Console.WriteLine("Part 1: {0}", fish.Sum());
 }
+Console.WriteLine("Part 2: {0}", fish.Sum());
